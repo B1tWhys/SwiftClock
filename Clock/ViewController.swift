89acff7280435c2e5070071ac3b255ef06e11ff3
Skyler Arnold <skyarnold1@me.com>
2016-01-22 09:29:46 -0700

    Initial Commit

---
+++ Clock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clock
+//
+//  Created by Skyler Arnold on 1/22/16.
+//  Copyright © 2016 Skyler Arnold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
